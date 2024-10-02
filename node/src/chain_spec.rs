e0523f32f4784f123da6e368adf45bfb5ab87563
Dastanbek Samatov <dastanbeksamatov@gmail.com>
2024-10-02 17:48:52 +0600

    Fix staking not enough stash

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,22 +1,24 @@
-use serde_json::json;
-use sp_core::{Pair, Public, sr25519, U256};
 use clover_runtime::{
-  wasm_binary_unwrap, AccountId, Balance, Block, ImOnlineId, SessionKeys, Signature, StakerStatus, DOLLARS
+    wasm_binary_unwrap, AccountId, Balance, Block, ImOnlineId, SessionKeys, Signature,
+    StakerStatus, DOLLARS,
 };
+use fp_evm::GenesisAccount;
+use hex_literal::hex;
+use primitive_types::H160;
+use sc_chain_spec::ChainSpecExtension;
+use sc_service::{ChainType, Properties};
+use sc_telemetry::TelemetryEndpoints;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
 use sp_consensus_babe::AuthorityId as BabeId;
 use sp_consensus_grandpa::AuthorityId as GrandpaId;
-use sp_runtime::{traits::{IdentifyAccount, Verify}, Perbill};
-use sc_service::{ChainType, Properties};
-use hex_literal::hex;
-use sc_telemetry::TelemetryEndpoints;
 use sp_core::crypto::UncheckedInto;
+use sp_core::{sr25519, Pair, Public, U256};
+use sp_runtime::traits::{IdentifyAccount, Verify};
+use sp_runtime::Perbill;
 use std::collections::BTreeMap;
-use fp_evm::GenesisAccount;
-use primitive_types::H160;
 use std::str::FromStr;
-use serde::{Serialize, Deserialize};
-use sc_chain_spec::ChainSpecExtension;
 
 // The URL for the telemetry server.
 const TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -28,160 +30,183 @@ const TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 #[derive(Default, Clone, Serialize, Deserialize, ChainSpecExtension)]
 #[serde(rename_all = "camelCase")]
 pub struct Extensions {
-	/// Block numbers with known hashes.
-	pub fork_blocks: sc_client_api::ForkBlocks<Block>,
-	/// Known bad block hashes.
-	pub bad_blocks: sc_client_api::BadBlocks<Block>,
-	/// The light sync state extension used by the sync-state rpc.
-	pub light_sync_state: sc_sync_state_rpc::LightSyncStateExtension,
+    /// Block numbers with known hashes.
+    pub fork_blocks: sc_client_api::ForkBlocks<Block>,
+    /// Known bad block hashes.
+    pub bad_blocks: sc_client_api::BadBlocks<Block>,
+    /// The light sync state extension used by the sync-state rpc.
+    pub light_sync_state: sc_sync_state_rpc::LightSyncStateExtension,
 }
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec<Extensions>;
 
 fn session_keys(
-  grandpa: GrandpaId,
-  babe: BabeId,
-  im_online: ImOnlineId,
-  authority_discovery: AuthorityDiscoveryId,
+    grandpa: GrandpaId,
+    babe: BabeId,
+    im_online: ImOnlineId,
+    authority_discovery: AuthorityDiscoveryId,
 ) -> SessionKeys {
-  SessionKeys { grandpa, babe, im_online, authority_discovery, }
+    SessionKeys {
+        grandpa,
+        babe,
+        im_online,
+        authority_discovery,
+    }
 }
 
 /// Generate a crypto pair from seed.
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-  TPublic::Pair::from_string(&format!("//{}", seed), None)
-    .expect("static values are valid; qed")
-    .public()
+    TPublic::Pair::from_string(&format!("//{}", seed), None)
+        .expect("static values are valid; qed")
+        .public()
 }
 
 type AccountPublic = <Signature as Verify>::Signer;
 
 /// Generate an account ID from seed.
-pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
-  AccountPublic: From<<TPublic::Pair as Pair>::Public>
+pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
+where
+    AccountPublic: From<<TPublic::Pair as Pair>::Public>,
 {
-  AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+    AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
 /// Generate an Babe authority key.
-pub fn authority_keys_from_seed(s: &str) -> (AccountId, AccountId, BabeId, GrandpaId, ImOnlineId, AuthorityDiscoveryId) {
-  (
-    get_account_id_from_seed::<sr25519::Public>(&format!("{}//stash", s)),
-    get_account_id_from_seed::<sr25519::Public>(s),
-    get_from_seed::<BabeId>(s),
-    get_from_seed::<GrandpaId>(s),
-    get_from_seed::<ImOnlineId>(s),
-    get_from_seed::<AuthorityDiscoveryId>(s),
-  )
+pub fn authority_keys_from_seed(
+    s: &str,
+) -> (
+    AccountId,
+    AccountId,
+    BabeId,
+    GrandpaId,
+    ImOnlineId,
+    AuthorityDiscoveryId,
+) {
+    (
+        get_account_id_from_seed::<sr25519::Public>(&format!("{}//stash", s)),
+        get_account_id_from_seed::<sr25519::Public>(s),
+        get_from_seed::<BabeId>(s),
+        get_from_seed::<GrandpaId>(s),
+        get_from_seed::<ImOnlineId>(s),
+        get_from_seed::<AuthorityDiscoveryId>(s),
+    )
 }
 
-fn endowed_evm_account() -> BTreeMap<H160, GenesisAccount>{
-  let endowed_account = vec![
-    // clover fauct
-    H160::from_str("9157f0316f375e4ccf67f8d21ae0881d0abcbb21").unwrap()
-  ];
-  get_endowed_evm_accounts(endowed_account)
+fn endowed_evm_account() -> BTreeMap<H160, GenesisAccount> {
+    let endowed_account = vec![
+        // clover fauct
+        H160::from_str("9157f0316f375e4ccf67f8d21ae0881d0abcbb21").unwrap(),
+    ];
+    get_endowed_evm_accounts(endowed_account)
 }
 
-fn dev_endowed_evm_accounts() -> BTreeMap<H160, GenesisAccount>{
-  let endowed_account = vec![
-    H160::from_str("6be02d1d3665660d22ff9624b7be0551ee1ac91b").unwrap(),
-    H160::from_str("e6206C7f064c7d77C6d8e3eD8601c9AA435419cE").unwrap(),
-    // the dev account key
-    // seed: bottom drive obey lake curtain smoke basket hold race lonely fit walk
-    // private key: 0x03183f27e9d78698a05c24eb6732630eb17725fcf2b53ee3a6a635d6ff139680
-    H160::from_str("aed40f2261ba43b4dffe484265ce82d8ffe2b4db").unwrap()
-  ];
+fn dev_endowed_evm_accounts() -> BTreeMap<H160, GenesisAccount> {
+    let endowed_account = vec![
+        H160::from_str("6be02d1d3665660d22ff9624b7be0551ee1ac91b").unwrap(),
+        H160::from_str("e6206C7f064c7d77C6d8e3eD8601c9AA435419cE").unwrap(),
+        // the dev account key
+        // seed: bottom drive obey lake curtain smoke basket hold race lonely fit walk
+        // private key: 0x03183f27e9d78698a05c24eb6732630eb17725fcf2b53ee3a6a635d6ff139680
+        H160::from_str("aed40f2261ba43b4dffe484265ce82d8ffe2b4db").unwrap(),
+    ];
 
-  get_endowed_evm_accounts(endowed_account)
+    get_endowed_evm_accounts(endowed_account)
 }
 
-fn get_endowed_evm_accounts(endowed_account: Vec<H160>) -> BTreeMap<H160, GenesisAccount>{
-  let mut evm_accounts = BTreeMap::new();
-  for account in endowed_account {
-    evm_accounts.insert(
-      account,
-      GenesisAccount {
-        nonce: U256::from(0),
-        balance: U256::from(1_000 * DOLLARS),
-        storage: Default::default(),
-        code: vec![],
-      },
-    );
-  }
-  evm_accounts
+fn get_endowed_evm_accounts(endowed_account: Vec<H160>) -> BTreeMap<H160, GenesisAccount> {
+    let mut evm_accounts = BTreeMap::new();
+    for account in endowed_account {
+        evm_accounts.insert(
+            account,
+            GenesisAccount {
+                nonce: U256::from(0),
+                balance: U256::from(1_000 * DOLLARS),
+                storage: Default::default(),
+                code: vec![],
+            },
+        );
+    }
+    evm_accounts
 }
 
 pub fn development_config() -> ChainSpec {
-  ChainSpec::builder(wasm_binary_unwrap(), Default::default())
-    .with_name("Development")
-    .with_id("dev")
-    .with_chain_type(ChainType::Development)
-    .with_protocol_id("cloverlocal")
-    .with_properties(json!({
-      "tokenDecimals": 18,
-      "tokenSymbol": "CLV"
-    }).as_object().expect("Created an object").clone())
-    .with_genesis_config_patch(testnet_genesis(
-      // Initial PoA authorities
-      vec![
-        authority_keys_from_seed("Alice"),
-      ],
-      // Sudo account
-      get_account_id_from_seed::<sr25519::Public>("Alice"),
-      // Pre-funded accounts
-      vec![
-        get_account_id_from_seed::<sr25519::Public>("Alice"),
-        get_account_id_from_seed::<sr25519::Public>("Bob"),
-        //get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-      ],
-      dev_endowed_evm_accounts()
-      ))
-    .build()
+    ChainSpec::builder(wasm_binary_unwrap(), Default::default())
+        .with_name("Development")
+        .with_id("dev")
+        .with_chain_type(ChainType::Development)
+        .with_protocol_id("cloverlocal")
+        .with_properties(
+            json!({
+              "tokenDecimals": 18,
+              "tokenSymbol": "CLV"
+            })
+            .as_object()
+            .expect("Created an object")
+            .clone(),
+        )
+        .with_genesis_config_patch(testnet_genesis(
+            // Initial PoA authorities
+            vec![authority_keys_from_seed("Alice")],
+            // Sudo account
+            get_account_id_from_seed::<sr25519::Public>("Alice"),
+            // Pre-funded accounts
+            vec![
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
+                get_account_id_from_seed::<sr25519::Public>("Bob"),
+                //get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+            ],
+            dev_endowed_evm_accounts(),
+        ))
+        .build()
 }
 
 pub fn local_testnet_config() -> ChainSpec {
-  ChainSpec::builder(wasm_binary_unwrap(), Default::default())
-    .with_name("Clover")
-    .with_id("local_testnet")
-    .with_chain_type(ChainType::Local)
-    .with_protocol_id("cloverlocal")
-    .with_properties(json!({
-      "tokenDecimals": 18,
-      "tokenSymbol": "CLV"
-    }).as_object().expect("Created an object").clone())
-    .with_genesis_config_patch(testnet_genesis(
-      // Initial PoA authorities
-      vec![
-        authority_keys_from_seed("Alice"),
-        authority_keys_from_seed("Bob"),
-      ],
-      // Sudo account
-      get_account_id_from_seed::<sr25519::Public>("Alice"),
-      // Pre-funded accounts
-      vec![
-        get_account_id_from_seed::<sr25519::Public>("Alice"),
-        get_account_id_from_seed::<sr25519::Public>("Bob"),
-        get_account_id_from_seed::<sr25519::Public>("Charlie"),
-        get_account_id_from_seed::<sr25519::Public>("Dave"),
-        get_account_id_from_seed::<sr25519::Public>("Eve"),
-        get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-        //get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-        //get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-        get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-      ],
-      endowed_evm_account()
-    ))
-    .build()
+    ChainSpec::builder(wasm_binary_unwrap(), Default::default())
+        .with_name("Clover")
+        .with_id("local_testnet")
+        .with_chain_type(ChainType::Local)
+        .with_protocol_id("cloverlocal")
+        .with_properties(
+            json!({
+              "tokenDecimals": 18,
+              "tokenSymbol": "CLV"
+            })
+            .as_object()
+            .expect("Created an object")
+            .clone(),
+        )
+        .with_genesis_config_patch(testnet_genesis(
+            // Initial PoA authorities
+            vec![
+                authority_keys_from_seed("Alice"),
+                authority_keys_from_seed("Bob"),
+            ],
+            // Sudo account
+            get_account_id_from_seed::<sr25519::Public>("Alice"),
+            // Pre-funded accounts
+            vec![
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
+                get_account_id_from_seed::<sr25519::Public>("Bob"),
+                get_account_id_from_seed::<sr25519::Public>("Charlie"),
+                get_account_id_from_seed::<sr25519::Public>("Dave"),
+                get_account_id_from_seed::<sr25519::Public>("Eve"),
+                get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+                //get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+                //get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+            ],
+            endowed_evm_account(),
+        ))
+        .build()
 }
 
 pub fn local_rose_testnet_config() -> ChainSpec {
-  ChainSpec::builder(wasm_binary_unwrap(), Default::default())
+    ChainSpec::builder(wasm_binary_unwrap(), Default::default())
     .with_name("Clover")
     .with_id("rose")
     .with_chain_type(ChainType::Custom("rose".to_string()))
@@ -261,7 +286,7 @@ pub fn local_rose_testnet_config() -> ChainSpec {
 }
 
 pub fn iris_testnet_config() -> ChainSpec {
-  ChainSpec::builder(wasm_binary_unwrap(), Default::default())
+    ChainSpec::builder(wasm_binary_unwrap(), Default::default())
   .with_name("Clover")
   .with_id("iris")
   .with_chain_type(ChainType::Custom("iris".to_string()))
@@ -341,7 +366,7 @@ pub fn iris_testnet_config() -> ChainSpec {
 }
 
 pub fn ivy_config() -> ChainSpec {
-  ChainSpec::builder(wasm_binary_unwrap(), Default::default())
+    ChainSpec::builder(wasm_binary_unwrap(), Default::default())
     .with_name("Clover Mainnet")
     .with_id("clover_ivy")
     .with_chain_type(ChainType::Live)
@@ -423,51 +448,51 @@ pub fn ivy_config() -> ChainSpec {
 
 /// Configure initial storage state for FRAME modules.
 pub fn testnet_genesis(
-	initial_authorities: Vec<(
-		AccountId,
-		AccountId,
-		BabeId,
-		GrandpaId,
-		ImOnlineId,
-		AuthorityDiscoveryId,
-	)>,
-	root_key: AccountId,
-	endowed_accounts: Vec<AccountId>,
-  endowed_eth_accounts: BTreeMap<H160, GenesisAccount>,
+    initial_authorities: Vec<(
+        AccountId,
+        AccountId,
+        BabeId,
+        GrandpaId,
+        ImOnlineId,
+        AuthorityDiscoveryId,
+    )>,
+    root_key: AccountId,
+    endowed_accounts: Vec<AccountId>,
+    endowed_eth_accounts: BTreeMap<H160, GenesisAccount>,
 ) -> serde_json::Value {
-  const ENDOWMENT: Balance = 1_000 * DOLLARS;
-  const STASH: Balance = 100 * DOLLARS;
-  const AUTHOR_BALANCE: Balance = 200 * DOLLARS;
+    const ENDOWMENT: Balance = 1_000 * DOLLARS;
+    const STASH: Balance = 100 * DOLLARS;
+    const AUTHOR_BALANCE: Balance = 200 * DOLLARS;
 
-  serde_json::json!({
-    "balances": {
-      "balances": endowed_accounts.iter().cloned().map(|k| (k, ENDOWMENT)).collect::<Vec<_>>(),
-    },
-    "session": {
-      "keys": initial_authorities.iter()
-        .map(|x| (x.0.clone(), x.0.clone(), session_keys(
-          x.3.clone(),
-          x.2.clone(),
-          x.4.clone(),
-          x.5.clone(),
-        )))
-				.collect::<Vec<_>>(),
-    },
-    "staking": {
-      "validatorCount": initial_authorities.len() as u32,
-      "minimumValidatorCount": initial_authorities.len() as u32,
-      "invulnerables": initial_authorities.iter().map(|x| x.0.clone()).collect::<Vec<_>>(),
-      "slashRewardFraction": Perbill::from_percent(10),
-      "stakers":  initial_authorities.iter().map(|x| {
-        (x.0.clone(), x.0.clone(), STASH, StakerStatus::<AccountId>::Validator)
-      }).collect::<Vec<_>>(),
-    },
-    "babe": {
-      "epochConfig": Some(clover_runtime::BABE_GENESIS_EPOCH_CONFIG),
-    },
-		"sudo": { "key": Some(root_key.clone()) },
-    "evm": {
-      "accounts": Some(endowed_eth_accounts),
-    }
-  })
+    serde_json::json!({
+      "balances": {
+        "balances": endowed_accounts.iter().chain(initial_authorities.iter().map(|a| &a.0)).map(|k| (k, ENDOWMENT)).collect::<Vec<_>>(),
+      },
+      "session": {
+        "keys": initial_authorities.iter()
+          .map(|x| (x.0.clone(), x.0.clone(), session_keys(
+            x.3.clone(),
+            x.2.clone(),
+            x.4.clone(),
+            x.5.clone(),
+          )))
+                  .collect::<Vec<_>>(),
+      },
+      "staking": {
+        "validatorCount": initial_authorities.len() as u32,
+        "minimumValidatorCount": initial_authorities.len() as u32,
+        "invulnerables": initial_authorities.iter().map(|x| x.0.clone()).collect::<Vec<_>>(),
+        "slashRewardFraction": Perbill::from_percent(10),
+        "stakers":  initial_authorities.iter().map(|x| {
+          (x.0.clone(), x.0.clone(), STASH, StakerStatus::<AccountId>::Validator)
+        }).collect::<Vec<_>>(),
+      },
+      "babe": {
+        "epochConfig": Some(clover_runtime::BABE_GENESIS_EPOCH_CONFIG),
+      },
+          "sudo": { "key": Some(root_key.clone()) },
+      "evm": {
+        "accounts": Some(endowed_eth_accounts),
+      }
+    })
 }
